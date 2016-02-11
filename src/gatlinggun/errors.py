56c117c307026503f36fc9d3535f05e2a5e4cc17
Andrey Vasilenkov <indigo@yandex-team.ru>
2016-02-11 16:45:56 +0300

    Refactor elliptics errors processing

--- src/gatlinggun/errors.py
+++ src/gatlinggun/errors.py
@@ -12,3 +12,11 @@ class InvalidDataError(Exception):
 class EmptyQueue(Exception):
     """Empty queue event, not actually an error"""
     pass
+
+
+class EllipticsError(Exception):
+    def __init__(self, elliptics_error_code):
+        self.elliptics_error_code = elliptics_error_code
+
+    def __str__(self):
+        return 'Elliptics error: {}'.format(self.elliptics_error_code)
